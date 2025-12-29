4387420b760a4b64f0633b9fb6a21d99610eb866
Praashon <prashon.social@gmail.com>
2025-12-29 17:09:25 +0545

    Cloud Storage, PDF-Image conversion, Key-Value Storage and AI Feedback

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,31 +1,81 @@
+import { prepareInstructions } from 'constants';
 import { useState, type FormEvent } from 'react'
+import { useNavigate } from 'react-router';
 import FileUploader from '~/components/FileUploader';
 import Navbar from '~/components/Navbar'
+import { convertPdfToImage } from '~/lib/pdf2img';
+import { usePuterStore } from '~/lib/puter';
+import { generateUUID } from '~/lib/utils';
 
 const Upload = () => {
+    const { auth, isLoading, fs, ai, kv } = usePuterStore();
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
-    const [statusText, StatusText] = useState('');
+    const [statusText, setStatusText] = useState('');
     const [file, setFile] = useState<File | null>(null)
 
     const handleFileSelect = (file: File | null) => {
         setFile(file);
     }
+
+    const handleAnalyze = async ({ companyName, jobTitle, jobDescription, file }: { companyName: string, jobTitle: string, jobDescription: string, file: File }) => {
+        setIsProcessing(true);
+        setStatusText('Analysing your resume...');
+
+        const uploadedFile = await fs.upload([file]);
+        if (!uploadedFile) return setStatusText('Error: Failed to upload file');
+
+        setStatusText('Converting to image ...');
+        const imageFile = await convertPdfToImage(file);
+        if (!imageFile.file) return setStatusText("Error: Failed to convert PDF to image.")
+
+        setStatusText('Uploading the image...')
+        const uploadedImage = await fs.upload([imageFile.file]);
+        if (!uploadedImage) return setStatusText('Error: Failed to upload image');
+
+        setStatusText('Preparing data...')
+        const uuid = generateUUID();
+
+        const data = {
+            id: uuid,
+            resumePath: uploadedFile.path,
+            imagePath: uploadedImage.path,
+            companyName,
+            jobTitle,
+            jobDescription,
+            feedback: '',
+        }
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+        setStatusText('Analysing...');
+
+        const feedback = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({ jobTitle, jobDescription })
+        )
+        if (!feedback) return setStatusText('Error: Failed to Analyze Resume.')
+
+        const feedbackText = typeof feedback.message.content === 'string' ? feedback.message.content : feedback.message.content[0].text;
+
+        data.feedback = JSON.parse(feedbackText);
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+        setStatusText('Analysis completed, redirecting...');
+        console.log(data);
+    }
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const form = e.currentTarget.closest('form');
         if (!form) return;
         const formData = new FormData(form);
 
-        const companyName = formData.get('company-name');
-        const jobDescription = formData.get('job-description');
-        const jobTitle = formData.get('job-title');
+        const companyName = formData.get('company-name') as string;
+        const jobDescription = formData.get('job-description') as string;
+        const jobTitle = formData.get('job-title') as string;
 
-        console.log({
-            companyName,
-            jobDescription,
-            jobTitle,
-            file
-        })
+        if (!file) return;
+
+        handleAnalyze({ companyName, jobTitle, jobDescription, file });
     }
 
     return (
@@ -62,7 +112,7 @@ const Upload = () => {
                                 <FileUploader onFileSelect={handleFileSelect} />
                             </div>
                             <button className='primary-button' type='submit'>
-                                Analyse Resume
+                                AnalyZe Resume
                             </button>
                         </form>
                     )}
@@ -71,5 +121,6 @@ const Upload = () => {
         </main>
     )
 }
+
 
 export default Upload
